12d75007f96cbffb7c03216fb7ee4af0cdec08dd
Rizwan Reza <rizwanreza@gmail.com>
2010-05-19 15:37:03 +0430

    Unstyled app.

---
+++ towerwatch.rb
@@ -0,0 +1,45 @@
+require "rubygems"
+require "sinatra"
+require "haml"
+require "lighthouse-api"
+require "yaml"
+
+config =  YAML::load( File.read(File.expand_path( '../config/credentials.yml', __FILE__ )) )
+
+set :username, config['username']
+set :password, config['password']
+
+before do
+  headers "Content-Type" => "text/html; charset=utf-8"
+  Lighthouse.account = "rails"
+  @project = Lighthouse::Project.find(8994)
+end
+
+get '/?' do
+  if params[:q]
+    @tickets = @project.tickets(:q => params[:q])
+    unless params[:page]
+      params[:page] = 2
+    else
+      params[:page] = params[:page] + 1
+    end
+    @page_link = "/?q=#{params[:q]}?page=#{params[:page]}"
+    haml :tickets
+  else
+    haml :search
+  end
+end
+
+post '/prioritize' do
+  if params[:priority] != ''
+    Lighthouse.authenticate(settings.username, settings.password)
+    ticket = Lighthouse::Ticket.find(params[:ticket_id], :params => { :project_id => 8994 })
+    ticket.priority = params[:priority].to_i
+    if ticket.save
+      "Submitted #{params[:priority]} priority for ticket ID: #{params[:ticket_id]}"
+    end
+  else
+    "No priority chosen"
+  end
+end
+
